d9ba482c1bf951630d84286da8900f6ee683ed8e
nawhi <25394965+nawhi@users.noreply.github.com>
2019-05-03 17:10:37 +0100

    Html converter: goldenmasterish test

--- Java/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
+++ Java/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
@@ -4,10 +4,23 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.io.IOException;
+
 public class HtmlTextConverterTest {
+
     @Test
-    public void foo() {
+    public void returns_its_filename() {
         HtmlTextConverter converter = new HtmlTextConverter("foo");
-        assertEquals("fixme", converter.getFilename());
+        assertEquals("foo", converter.getFilename());
+    }
+
+    @Test
+    public void converts_a_unicode_file() throws IOException {
+        String fullFilenameWithPath = HtmlTextConverterTest.class
+                .getResource("/foo.txt").getPath();
+        HtmlTextConverter converter = new HtmlTextConverter(fullFilenameWithPath);
+
+        String expectedHtml = "hello<br />world<br />&amp;<br />&lt;<br />&gt;<br />&quot;<br />&quot;<br />";
+        assertEquals(expectedHtml, converter.convertToHtml());
     }
 }
